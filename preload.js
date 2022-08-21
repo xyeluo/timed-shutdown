604ad0c58a4724987f34d8505641cee452e1c4e5
tk914 <2978797310@qq.com>
2022-08-21 11:59:29 +0800

    添加注释

--- preload.js
+++ preload.js
@@ -8,6 +8,10 @@ const { exec } = require('child_process'),
 
 iconv.skipDecodeWarning = true; //忽略warining
 
+/**
+ * @Description: 添加计划任务，执行关机、重启、休眠
+ * @param {string} command 用schtasks命令，由用户指定执行任务的时间
+ */
 function execCmd(command) {
   return new Promise((resolve, reject) => {
     exec(command, { encoding: binaryEncoding }, (error, stdout, stderr) => {
@@ -21,10 +25,12 @@ function execCmd(command) {
   })
 }
 
+// 数据本地存储
 function dbStorageSave(value) {
   utools.dbStorage.setItem(Id, value);
 }
 
+// 读取本地数据
 function dbStorageRead() {
   const plans = utools.dbStorage.getItem(Id);
   if (plans === null) {
